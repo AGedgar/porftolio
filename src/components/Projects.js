f6b4d9554a6f030a6d6c5782293bc86eb905404b
AGedgar <edgar11_93@hotmail.com>
2022-07-28 20:27:54 -0500

    added alt to images in skills and projects setions

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -36,7 +36,7 @@ export const Projects = () =>{
                     </Col>
                 </Row>
             </Container>
-            <img className="background-image-right" src={colorSharp2} />
+            <img className="background-image-right" src={colorSharp2} alt="colorSharp" />
         </section>
     )
 };
